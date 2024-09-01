82c9eb9cec3b49bbab184f3698ac98bb256f3bc8
Santiago Lopez Vallejo <santiagolopezvallejo@gmail.com>
2024-09-01 11:11:05 -0500

    Add social media icons and texts changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
-import { Manrope } from 'next/font/google';
-import { cn } from '@/lib/utils';
-import './globals.css';
+import { Manrope } from 'next/font/google'
+import { cn } from '@/lib/utils'
+import './globals.css'
 
 const fontHeading = Manrope({
   subsets: ['latin'],
