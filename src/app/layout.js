83daa236335330516f80823d2a6b48e5a8823735
Emmus <emonsalvep38@gmail.com>
2024-09-01 15:48:08 -0500

    Configuraciones Iniciales del Proyecto y para el deploy

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,33 +1,36 @@
-// This is the root layout component for your Next.js app.
-// Learn more: https://nextjs.org/docs/app/building-your-application/routing/pages-and-layouts#root-layout-required
-import { Manrope } from 'next/font/google'
-import { cn } from '@/lib/utils'
-import './globals.css'
+import { Manrope } from 'next/font/google';
+import { cn } from '@/lib/utils';
+import './globals.css';
 
 const fontHeading = Manrope({
   subsets: ['latin'],
   display: 'swap',
   variable: '--font-heading',
-})
+});
 
 const fontBody = Manrope({
   subsets: ['latin'],
   display: 'swap',
   variable: '--font-body',
-})
+});
 
 export default function Layout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={cn(
-          'antialiased',
-          fontHeading.variable,
-          fontBody.variable
-        )}
-      >
-        {children}
-      </body>
-    </html>
-  )
+    <>
+      <link rel="icon" href="/favicon.ico" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <title>The Last Hope</title>
+      <html lang="en">
+        <body
+          className={cn(
+            'antialiased',
+            fontHeading.variable,
+            fontBody.variable
+          )}
+        >
+          {children}
+        </body>
+      </html>
+    </>
+  );
 }
